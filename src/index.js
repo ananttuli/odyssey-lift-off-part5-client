041f7f22c6a7f1acfb643fbd1a9742ecee385046
my account <study.uq@gmail.com>
2021-09-12 18:20:16 +1000

    dotenv config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const client = new ApolloClient({
   uri: process.env.URI || 'http://localhost:4000',
